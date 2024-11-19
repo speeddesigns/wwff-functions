3f90553cca0df45a39c76d6cbd52fa19f3452323
speeddesigns <speeddesigns@gmail.com>
2024-11-19 00:45:44 +0000

    oops

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
+import express from 'express';
+import logger from './utils/logger.js';
+import config from './config/index.js';
+import jobRouter from './routes/job.routes.js';
 import logger from './utils/logger';
 
 logger.info('Application starting...');
 
-// Rest of main.js code
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use('/api/jobs', jobRouter);
+
+// Start the Express server and listen on the configured port
+const port = config.get('app.port');
+app.listen(port, () => {
+  logger.info(`Server is listening on port ${port}`, {
+    environment: config.get('app.environment'),
+    appName: config.get('app.name')
+  });
+});
+
+export default app;
